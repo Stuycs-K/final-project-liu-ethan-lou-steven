583445bb2ea2f29c831cf0994d5458be25ef4957
Ethan Liu <eliu51@stuy.edu>
2024-05-22 21:02:11 -0400

    Moved classes to different files for easier management. Created Sprite.java.

--- Game/Block.java
+++ Game/Block.java
@@ -1,52 +1,38 @@
-class Obstacle {
-  private float xcor, ycor;
-  public Obstacle(float x, float y) {
-    xcor=x;
-    ycor=y;
-  }
-  public float getX() {
-    return xcor;
-  }
-  public float getY() {
-    return ycor;
-  }
-}
-
 class Block extends Obstacle {
   private boolean jumpPad;
-  private float height, width;
+  private float h, w;
   public Block(float xcor, float ycor) {
     super(xcor, ycor);
-    height=1.0;
-    width=1.0;
+    h=1;
+    w=1;
     jumpPad=false;
   }
   public Block(float xcor, float ycor, float w) {
     super(xcor, ycor);
-    height=w;
-    width=w;
+    this.h=w;
+    this.w=w;
     jumpPad=false;
   }
   public Block(float xcor, float ycor, float w, float h) {
     super(xcor, ycor);
-    height=h;
-    width=w;
+    this.h=h;
+    this.w=w;
     jumpPad=false;
   }
   public Block(float xcor, float ycor, float w, float h, boolean b) {
     super(xcor, ycor);
-    height=h;
-    width=w;
+    this.h=h;
+    this.w=w;
     jumpPad=b;
   }
-  public float getHeight(){return height;}
-  public float getWidth(){return width;}
+  public float getHeight(){return h;}
+  public float getWidth(){return w;}
   public boolean hasJumpPad(){return jumpPad;}
-  public int isTouching(float x, float y) {
+  //Takes in x and y coordinates of a point and determines if that point lies on or within the block. 0 - not touching. 1 - touching the left or right side. 2 - touching the top or bottom.
+  public int isTouching(Sprite s) {
     return 0;
   }
-}
-
-class Spike extends Obstacle {
-
+  public boolean isTouchingJump(Sprite s) {
+    return false;
+  }
 }
